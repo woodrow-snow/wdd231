4d4ba844c9ed0552c6e373aa3be4f94921e1df09
woodrow-snow <woodrow2021@outlook.com>
2025-11-15 12:35:33 -0700

    uploading current progress

--- chamber/scripts/build_business_card.mjs
+++ chamber/scripts/build_business_card.mjs
@@ -7,5 +7,36 @@ export function buildBusiness(bData, sectionIn){
     const website = document.createElement('p');
     const memLvl = document.createElement('p');
 
-    
+    // creating div for elements to go in
+    const bCard = document.createElement('div');
+    bCard.classList.add('business-card');
+
+    // populating data
+    name.innerHTML = bData.name;
+    phone.innerHTML = `<strong>PHONE:</strong> ${bData.phone_number}`;
+    address.innerHTML = `<strong>ADDRESS:</strong> ${bData.address} ${bData.city_state_zip}`;
+    website.innerHTML = `<strong>URL:</strong> ${bData.url}`;
+    memLvl.innerHTML = `<strong>Membership Level:</strong> ${bData.membership_lvl}`;
+
+    // creating image element and adding attributes
+    let imgLink;
+    if(bData.image_file_name == ''){
+        imgLink = 'business_default.svg';
+    }
+    else {
+        imgLink = bData.image_file_name;
+    }
+    logo.setAttribute('src',`images/${imgLink}`); // C:\Users\Owner\Desktop\School Documents\wdd231\chamber\images\business_default.svg
+    logo.setAttribute('alt', bData.name);
+
+    // adding elements to div
+    bCard.appendChild(name);
+    bCard.appendChild(logo);
+    bCard.appendChild(phone);
+    bCard.appendChild(address);
+    bCard.appendChild(website);
+    bCard.appendChild(memLvl);
+
+    // adding card to page
+    sectionIn.append(bCard);
 }
--- chamber/scripts/output.mjs
+++ chamber/scripts/output.mjs
@@ -4,29 +4,25 @@ import { buildWeatherCard } from "./build_weather.mjs";
 import { weatherURL, forecastURL, chamberURL }from "./urls.mjs";
 import { buildForecast } from "./build_forecast.mjs";
 import { pickMembers } from "./pick_members.mjs";
+import { buildBusiness } from "./build_business_card.mjs";
 
 // getting elements from document
 const currentWSection = document.querySelector('#weather-info');
 const forecastSection = document.querySelector('#forecast');
+const membersSection = document.querySelector('#spotlights');
 
 // building and deploying current weather card
 getData(weatherURL).then(weather => buildWeatherCard(currentWSection, weather));
 getData(forecastURL).then(forecast => buildForecast(forecastSection, forecast));
 
 
-// getting array of members to create cards for
-// const membersArray = getData(chamberURL).then(members => pickMembers(members))
-
-// membersArray.forEach(m => {
-//     console.log(m.membership_lvl);
-// });
-
+// getting array of members to create cards for them
 async function getMembers() {
     const membersData = await getData(chamberURL);
     const membersArray = pickMembers(membersData);
 
     membersArray.forEach(m => {
-        console.log(m.membership_lvl);
+        buildBusiness(m,membersSection);
     });
 }
 
